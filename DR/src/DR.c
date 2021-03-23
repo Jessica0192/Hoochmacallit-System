8d07b049ae085956706ba2ff28e933ab2453710c
MMalinina <72006658+MMalinina@users.noreply.github.com>
2021-03-22 23:08:57 -0400

    Update DR.c

--- DR/src/DR.c
+++ DR/src/DR.c
@@ -22,6 +22,8 @@ int removeDC(char* strCount, char*strProcessID, char* rem_dc_log, FILE* log_stre
 
 int main(void)
 {
+	//test
+	 MasterList* p;
 	int mid; // message queue ID
 	int sizeofdata;
 	int continueToRun = 1;
@@ -160,6 +162,13 @@ int main(void)
 	}
 
 	printf ("(PRODUCER) Our Shared-Memory ID is %d\n", shmid);
+	// p = (MasterList *)shmat (shmid, NULL, 0);
+	//	  if (p == NULL) 
+	//	  {
+	//	    printf ("(DX) Cannot attach to shared memory!\n");
+	//	    return 3;
+	//	  }
+	//	printf("\nP is %d\n", p);
 
 	msList = (MasterList *)shmat (shmid, NULL, 0);
 	if (msList == NULL) 
@@ -238,7 +247,7 @@ int main(void)
 
 	for (int i = 0; i < localNumDCs; i++)
 	{
-		if (masterls.dc[i].lastTimeHeardFrom.time >35){ 
+		if (msList->dc[i].lastTimeHeardFrom.time >35){ 
 			printf("\nMACHINE %d NON-RESPONSIVE\n", i);
 			removalID = i;
 			printf("\nREMOVAL ID IS %d WITH PID %d\n", removalID, DC_pids[removalID]);
@@ -255,7 +264,7 @@ int main(void)
 		localNumDCs--;
 		
 		
-		        masterls.numberOfDCs = localNumDCs;
+		        msList->numberOfDCs = localNumDCs;
 		        howManySec = 0; //reset
 		
 			sprintf(strCount, "%d", removalID);
@@ -275,13 +284,14 @@ int main(void)
 				for(int j = removalID; j < localNumDCs + 1; j++)
 				{
 					DC_pids[j] = DC_pids[j + 1];
-					masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
-					masterls.dc[j].lastTimeHeardFrom.time = masterls.dc[j + 1].lastTimeHeardFrom.time;
-					masterls.dc[j].lastTimeHeardFrom.hours = masterls.dc[j + 1].lastTimeHeardFrom.hours;
-					masterls.dc[j].lastTimeHeardFrom.minutes = masterls.dc[j + 1].lastTimeHeardFrom.minutes;
-					masterls.dc[j].lastTimeHeardFrom.seconds = masterls.dc[j + 1].lastTimeHeardFrom.seconds;
+					//masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
+					msList->dc[j].dcProcessID = msList->dc[j + 1].dcProcessID;
+					msList->dc[j].lastTimeHeardFrom.time = msList->dc[j + 1].lastTimeHeardFrom.time;
+					msList->dc[j].lastTimeHeardFrom.hours = msList->dc[j + 1].lastTimeHeardFrom.hours;
+					msList->dc[j].lastTimeHeardFrom.minutes = msList->dc[j + 1].lastTimeHeardFrom.minutes;
+					msList->dc[j].lastTimeHeardFrom.seconds = msList->dc[j + 1].lastTimeHeardFrom.seconds;
 					printf("\nNOW DC %d is %d\n", j, DC_pids[j]);
-					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , masterls.dc[j].dcProcessID);
+					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , msList->dc[j].dcProcessID);
 				}
 				DC_pids[localNumDCs + 1] = 0;
 			}
@@ -290,13 +300,14 @@ int main(void)
 				for(int j = removalID; j < localNumDCs; j++ )
 				{
 					DC_pids[j] = DC_pids[j + 1];
-					masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
-					masterls.dc[j].lastTimeHeardFrom.time = masterls.dc[j + 1].lastTimeHeardFrom.time;
-					masterls.dc[j].lastTimeHeardFrom.hours = masterls.dc[j + 1].lastTimeHeardFrom.hours;
-					masterls.dc[j].lastTimeHeardFrom.minutes = masterls.dc[j + 1].lastTimeHeardFrom.minutes;
-					masterls.dc[j].lastTimeHeardFrom.seconds = masterls.dc[j + 1].lastTimeHeardFrom.seconds;
+					//masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
+					msList->dc[j].dcProcessID = msList->dc[j + 1].dcProcessID;
+					msList->dc[j].lastTimeHeardFrom.time = msList->dc[j + 1].lastTimeHeardFrom.time;
+					msList->dc[j].lastTimeHeardFrom.hours = msList->dc[j + 1].lastTimeHeardFrom.hours;
+					msList->dc[j].lastTimeHeardFrom.minutes = msList->dc[j + 1].lastTimeHeardFrom.minutes;
+					msList->dc[j].lastTimeHeardFrom.seconds = msList->dc[j + 1].lastTimeHeardFrom.seconds;
 					printf("\nNOW DC %d is %d\n", j, DC_pids[j]);
-					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , masterls.dc[j].dcProcessID);
+					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , msList->dc[j].dcProcessID);
 				}
 				DC_pids[localNumDCs] = 0;
 			}
@@ -313,45 +324,29 @@ int main(void)
 			
 
 		
-          //      masterls.numberOfDCs = localNumDCs;
-            //    howManySec = 0; //reset
-		
-//		sprintf(strCount, "%d", masterls.numberOfDCs);
-//		//I CHANGED IT RNsprintf(strProcessID, "%d", incom_msg.machinePID);// masterls.dc[DC_count].dcProcessID);
-//
-//		sprintf(strProcessID, "%d",DC_pids[masterls.numberOfDCs]);
-//
-//		removeDC(strCount, strProcessID, rem_dc_log, log_stream);
+       
 
-		
-//		if (localNumDCs == 0)
-//		{
-//			char* allDCOffMsg = "All DCs have gone offline or terminated – DR TERMINATING";
-//			fprintf(log_stream, "%s", allDCOffMsg);
-//			break;
-//		}
 			
                }
 		if (localNumDCs == 1) 
 		{
 			index = 0;
 		}else{
-			index = masterls.numberOfDCs;
+			index = msList->numberOfDCs;
 		}
-		//printf("\nMINUTES from DC %d SECONDS from DC %d\n", masterls.dc[index].lastTimeHeardFrom.minutes, masterls.dc[index].lastTimeHeardFrom.seconds);
 
 
 		for (int i = 0; i < localNumDCs; i++)
 		{
-			masterls.dc[i].lastTimeHeardFrom.time = ((timeinfo->tm_min * 60) + timeinfo->tm_sec) - ((masterls.dc[i].lastTimeHeardFrom.minutes * 60) + masterls.dc[i].lastTimeHeardFrom.seconds);
-			if (masterls.dc[i].lastTimeHeardFrom.time > 35){
+			msList->dc[i].lastTimeHeardFrom.time = ((timeinfo->tm_min * 60) + timeinfo->tm_sec) - ((msList->dc[i].lastTimeHeardFrom.minutes * 60) + msList->dc[i].lastTimeHeardFrom.seconds);
+			if (msList->dc[i].lastTimeHeardFrom.time > 35){
 				//printf("\n35 SECONDS PASSED COMMON!\n"); 
 				howManySec = 35; 
 				break;
 			}
 		}
 		
-		if (((timeinfo->tm_min * 60) + timeinfo->tm_sec) - ((masterls.dc[index].lastTimeHeardFrom.minutes * 60) + masterls.dc[index].lastTimeHeardFrom.seconds) >35)
+		if (((timeinfo->tm_min * 60) + timeinfo->tm_sec) - ((msList->dc[index].lastTimeHeardFrom.minutes * 60) + msList->dc[index].lastTimeHeardFrom.seconds) >35)
 		{ howManySec = 35; break;}
 		
 	}}else {rc = msgrcv(mid, &incom_msg, sizeof (DCMessage) - sizeof (long),0 ,0);}
@@ -382,27 +377,12 @@ int main(void)
 	
 	for (int i = 0; i < localNumDCs; i++)
 	{if (incom_msg.machinePID == DC_pids[i]){index = i;}}
-    	masterls.dc[index].lastTimeHeardFrom.hours = timeinfo->tm_hour;
-	//printf("\nHOURS IS %d\n", masterls.dc[index].lastTimeHeardFrom.hours);
-    	masterls.dc[index].lastTimeHeardFrom.minutes = timeinfo->tm_min;
-	//printf("\nMINUTES IS %d\n", masterls.dc[index].lastTimeHeardFrom.minutes);
-    	masterls.dc[index].lastTimeHeardFrom.seconds = timeinfo->tm_sec;
-	//printf("\nSECONDS IS %d\n", masterls.dc[index].lastTimeHeardFrom.seconds);
-    //	printf("\nHERE %d\n", ((timeinfo->tm_min * 60) + timeinfo->tm_sec));
-   	//printf("\nAND HERE %d\n", ((cur_min * 60) + cur_sec));
-    	//printf("\nTHE ANSWER IS %d\n", howManySec);
-	//printf("incom_msg pid: %d\n", incom_msg.machinePID);
+    	msList->dc[index].lastTimeHeardFrom.hours = timeinfo->tm_hour;
+    	msList->dc[index].lastTimeHeardFrom.minutes = timeinfo->tm_min;
+    	msList->dc[index].lastTimeHeardFrom.seconds = timeinfo->tm_sec;
+	
 
-		
-	//NEW THING - Now we need to save the current DC with all its features to 			//the master list
-	//msList->dc[localNumDCs].dcProcessID = incom_msg.machinePID;
-	//printf("\nADDED %d MACHINE TO THE MASTER LIST\n", msList->dc[localNumDCs].dcProcessID);
-	//NEW ADDING LAST TIME HEARD FROM
-	//masterls.dc[localNumDCs].lastTimeHeardFrom.seconds += howManySec;
-	//printf("\nLAST TIME HEARD FROM DC %d is %d\n", localNumDCs, masterls.dc[localNumDCs].lastTimeHeardFrom.seconds);
-	//printf("incom_msg msg: %s\n", incom_msg.msg);
-	//printf("rc: %d\n", rc);
-	//printf("\nhow many sec is %d\n", howManySec);
+	
 	if (howManySec != 35){
         	if (rc == -1) 
         	{
@@ -418,7 +398,7 @@ int main(void)
 
 	for (int i = 0; i < localNumDCs; i++)
 	{
-		if (masterls.dc[i].lastTimeHeardFrom.time >35){ 
+		if (msList->dc[i].lastTimeHeardFrom.time >35){ 
 			printf("\nMACHINE %d NON-RESPONSIVE\n", i);
 			removalID = i;
 			printf("\nREMOVAL ID IS %d WITH PID %d\n", removalID, DC_pids[removalID]);
@@ -435,13 +415,12 @@ int main(void)
 		localNumDCs--;
 		
 		
-		        masterls.numberOfDCs = localNumDCs;
+		        msList->numberOfDCs = localNumDCs;
 		        howManySec = 0; //reset
 		
 			sprintf(strCount, "%d", removalID);
 			
 
-			//I CHANGED IT RNsprintf(strProcessID, "%d", incom_msg.machinePID);// masterls.dc[DC_count].dcProcessID);
 
 			sprintf(strProcessID, "%d",DC_pids[removalID]);
 
@@ -455,13 +434,14 @@ int main(void)
 				for(int j = removalID; j < localNumDCs + 1; j++)
 				{
 					DC_pids[j] = DC_pids[j + 1];
-					masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
-					masterls.dc[j].lastTimeHeardFrom.time = masterls.dc[j + 1].lastTimeHeardFrom.time;
-					masterls.dc[j].lastTimeHeardFrom.hours = masterls.dc[j + 1].lastTimeHeardFrom.hours;
-					masterls.dc[j].lastTimeHeardFrom.minutes = masterls.dc[j + 1].lastTimeHeardFrom.minutes;
-					masterls.dc[j].lastTimeHeardFrom.seconds = masterls.dc[j + 1].lastTimeHeardFrom.seconds;
+					//masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
+					msList->dc[j].dcProcessID = msList->dc[j + 1].dcProcessID;
+					msList->dc[j].lastTimeHeardFrom.time = msList->dc[j + 1].lastTimeHeardFrom.time;
+					msList->dc[j].lastTimeHeardFrom.hours = msList->dc[j + 1].lastTimeHeardFrom.hours;
+					msList->dc[j].lastTimeHeardFrom.minutes = msList->dc[j + 1].lastTimeHeardFrom.minutes;
+					msList->dc[j].lastTimeHeardFrom.seconds = msList->dc[j + 1].lastTimeHeardFrom.seconds;
 					printf("\nNOW DC %d is %d\n", j, DC_pids[j]);
-					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , masterls.dc[j].dcProcessID);
+					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , msList->dc[j].dcProcessID);
 				}
 				DC_pids[localNumDCs + 1] = 0;
 			}
@@ -470,13 +450,14 @@ int main(void)
 				for(int j = removalID; j < localNumDCs; j++ )
 				{
 					DC_pids[j] = DC_pids[j + 1];
-					masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
-					masterls.dc[j].lastTimeHeardFrom.time = masterls.dc[j + 1].lastTimeHeardFrom.time;
-					masterls.dc[j].lastTimeHeardFrom.hours = masterls.dc[j + 1].lastTimeHeardFrom.hours;
-					masterls.dc[j].lastTimeHeardFrom.minutes = masterls.dc[j + 1].lastTimeHeardFrom.minutes;
-					masterls.dc[j].lastTimeHeardFrom.seconds = masterls.dc[j + 1].lastTimeHeardFrom.seconds;
+					//masterls.dc[j].dcProcessID = masterls.dc[j + 1].dcProcessID;
+					msList->dc[j].dcProcessID = msList->dc[j + 1].dcProcessID;
+					msList->dc[j].lastTimeHeardFrom.time = msList->dc[j + 1].lastTimeHeardFrom.time;
+					msList->dc[j].lastTimeHeardFrom.hours = msList->dc[j + 1].lastTimeHeardFrom.hours;
+					msList->dc[j].lastTimeHeardFrom.minutes = msList->dc[j + 1].lastTimeHeardFrom.minutes;
+					msList->dc[j].lastTimeHeardFrom.seconds = msList->dc[j + 1].lastTimeHeardFrom.seconds;
 					printf("\nNOW DC %d is %d\n", j, DC_pids[j]);
-					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , masterls.dc[j].dcProcessID);
+					printf("\nAND IN THE MASTER LIST DC %d is %d\n", j , msList->dc[j].dcProcessID);
 				}
 				DC_pids[localNumDCs] = 0;
 			}
@@ -493,37 +474,21 @@ int main(void)
 			
 
 		
-          //      masterls.numberOfDCs = localNumDCs;
-            //    howManySec = 0; //reset
-		
-//		sprintf(strCount, "%d", masterls.numberOfDCs);
-//		//I CHANGED IT RNsprintf(strProcessID, "%d", incom_msg.machinePID);// masterls.dc[DC_count].dcProcessID);
-//
-//		sprintf(strProcessID, "%d",DC_pids[masterls.numberOfDCs]);
-//
-//		removeDC(strCount, strProcessID, rem_dc_log, log_stream);
-
-		
-//		if (localNumDCs == 0)
-//		{
-//			char* allDCOffMsg = "All DCs have gone offline or terminated – DR TERMINATING";
-//			fprintf(log_stream, "%s", allDCOffMsg);
-//			break;
-//		}
+ 
 			
                }
             //}
             else
             {
 		
-                masterls.msgQueueID = mid;
+                msList->msgQueueID = mid;
 		//COMMENTED OUT localNumDCs++;
 		//commented out masterls.numberOfDCs = localNumDCs;
 		//printf("numbofdc: %d\n", masterls.numberOfDCs);
                 //pid t DCMessage.ID
                //COMMENTED OUT THIS JUST NOW 12 08 AM 20 MARCH masterls.dc[DC_count].dcProcessID = (pid_t) incom_msg.machinePID; //what to do here?
 
-		sprintf(strCount, "%d", masterls.numberOfDCs);
+		sprintf(strCount, "%d", msList->numberOfDCs);
 		//NEW COMMENTED OUT sprintf(strProcessID, "%d", masterls.dc[DC_count].dcProcessID);
 		//printf("\nTHE DC COUNT IS %d and numbofdc is %d\n", DC_count, masterls.numberOfDCs);
 
@@ -533,9 +498,9 @@ int main(void)
 			{
 				for (int i = 0; i < localNumDCs + 1; i ++)
 				{
-					printf("\nmasterls.dc[%d].dcProcessID is %d\n", i, masterls.dc[i].dcProcessID);
+					printf("\nmasterls.dc[%d].dcProcessID is %d\n", i, msList->dc[i].dcProcessID);
 					printf("\nincom_msg.machinePID is %d\n", incom_msg.machinePID);
-					if ((pid_t)masterls.dc[i].dcProcessID == (pid_t)incom_msg.machinePID)
+					if ((pid_t)msList->dc[i].dcProcessID == (pid_t)incom_msg.machinePID)
 					{
 						existDCOrNot = true;
 					}
@@ -545,9 +510,9 @@ int main(void)
 			{
 				for (int i = 0; i < localNumDCs; i ++)
 				{
-					printf("\nmasterls.dc[i].dcProcessID is %d\n", masterls.dc[i].dcProcessID);
+					printf("\nmasterls.dc[i].dcProcessID is %d\n", msList->dc[i].dcProcessID);
 					printf("\nincom_msg.machinePID is %d\n", incom_msg.machinePID);
-					if ((pid_t)masterls.dc[i].dcProcessID == (pid_t)incom_msg.machinePID)
+					if ((pid_t)msList->dc[i].dcProcessID == (pid_t)incom_msg.machinePID)
 					{
 						existDCOrNot = true;
 					}
@@ -557,29 +522,27 @@ int main(void)
 
 		if (existDCOrNot == true){
 		//existDCOrNot = false; //WAIT WHAT
-		printf("\nFOR THIS %d PROCESS ID, the exist or not is TRUE\n", masterls.dc[localNumDCs].dcProcessID);
+		printf("\nFOR THIS %d PROCESS ID, the exist or not is TRUE\n", msList->dc[localNumDCs].dcProcessID);
 		}
 		else
 		{
-			printf("\nFOR THIS %d PROCESS ID, the exist or not is FALSE\n", masterls.dc[localNumDCs].dcProcessID);
+			printf("\nFOR THIS %d PROCESS ID, the exist or not is FALSE\n", msList->dc[localNumDCs].dcProcessID);
 		}
 
 		//NEW THING - THERE WAS NO IF STATEMENT
 		if (existDCOrNot == false)
 		{
 			//NEW FOR MACHINE
-		masterls.dc[localNumDCs].lastTimeHeardFrom.time = 0;
-			//sprintf(strProcessID, "%f", incom_msg.machinePID);//NEW - ADDED LINE HERE
+		msList->dc[localNumDCs].lastTimeHeardFrom.time = 0;
+			msList->dc[localNumDCs].dcProcessID = (pid_t) incom_msg.machinePID;
 			//LINE BELOW CHANGED FROM DC COUNT TO LOCALNUM
-			masterls.dc[localNumDCs].dcProcessID = (pid_t) incom_msg.machinePID;//NEW ADDED LLINE'
-			//LINE BELOW CHANGED FROM DC COUNT TO LOCALNUM
-			sprintf(strProcessID, "%d", masterls.dc[localNumDCs].dcProcessID);//NEW ADDED LINE
-			DC_pids[localNumDCs] = masterls.dc[localNumDCs].dcProcessID;
+			sprintf(strProcessID, "%d", msList->dc[localNumDCs].dcProcessID);//NEW ADDED LINE
+			DC_pids[localNumDCs] = msList->dc[localNumDCs].dcProcessID;
 
 			//printf("\nLOCAL NUM OF DCS BEFORE INCREMENTING %d\n", localNumDCs);
 			localNumDCs++;
 			//printf("\nLOCAL NUM OF DCS AFTER INCREMENTING %d\n", localNumDCs);
-			masterls.numberOfDCs = localNumDCs;
+			msList->numberOfDCs = localNumDCs;
 			time_t t = time(NULL);
 		    	struct tm tm = *localtime(&t);
 		    	//printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
@@ -604,15 +567,12 @@ int main(void)
 		    	asctime(timeinfo);
 
 			
-		    	masterls.dc[(localNumDCs-1)].lastTimeHeardFrom.hours = timeinfo->tm_hour;
-			//printf("\nHOURS IS %d\n", masterls.dc[index].lastTimeHeardFrom.hours);
-		    	masterls.dc[(localNumDCs-1)].lastTimeHeardFrom.minutes = timeinfo->tm_min;
-			//printf("\nMINUTES IS %d\n", masterls.dc[index].lastTimeHeardFrom.minutes);
-		    	masterls.dc[(localNumDCs-1)].lastTimeHeardFrom.seconds = timeinfo->tm_sec;
+		    	msList->dc[(localNumDCs-1)].lastTimeHeardFrom.hours = timeinfo->tm_hour;
+		    	msList->dc[(localNumDCs-1)].lastTimeHeardFrom.minutes = timeinfo->tm_min;
+		    	msList->dc[(localNumDCs-1)].lastTimeHeardFrom.seconds = timeinfo->tm_sec;
 		}
 		else if (rc >= 0)//NEW ELSE
 		{
-			//masterls.dc[localNumDCs].dcProcessID = (pid_t) incom_msg.machinePID;//NEW ADDED LLINE'
 			int cur_dc_id = 0;
 			//searching for the current DC's id 
 			for (int i = 0; i < 10; i++)
@@ -624,19 +584,13 @@ int main(void)
 				}
 			}
 			//LINE BELOW CHANGED FROM DC COUNT TO LOCALNUM
-			sprintf(strProcessID, "%d", incom_msg.machinePID);//WAS masterls.dc[localNumDCs].dcProcessID
-			//DC_pids[localNumDCs] = masterls.dc[localNumDCs].dcProcessID;
-
-			//printf("\nLOCAL NUM OF DCS BEFORE INCREMENTING %d\n", localNumDCs);
-			//localNumDCs++;
-			//printf("\nLOCAL NUM OF DCS AFTER INCREMENTING %d\n", localNumDCs);
-			//masterls.numberOfDCs = localNumDCs;
+			sprintf(strProcessID, "%d", incom_msg.machinePID);
 			time_t t = time(NULL);
 		    	struct tm tm = *localtime(&t);
 		    	//printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 			sprintf(strCount, "%d", (cur_dc_id));
 			strcat(upd_dc_log, strProcessID); //strProcessID
-			masterls.dc[cur_dc_id].lastTimeHeardFrom.time = 0;
+			msList->dc[cur_dc_id].lastTimeHeardFrom.time = 0;
 			
 			if (strcmp(incom_msg.msg, status0msg) == 0)
 			{
@@ -708,18 +662,14 @@ int main(void)
 		    	asctime(timeinfo);
 
 			
-		    	masterls.dc[cur_dc_id].lastTimeHeardFrom.hours = timeinfo->tm_hour;
-			//printf("\nHOURS IS %d\n", masterls.dc[index].lastTimeHeardFrom.hours);
-		    	masterls.dc[cur_dc_id].lastTimeHeardFrom.minutes = timeinfo->tm_min;
-			//printf("\nMINUTES IS %d\n", masterls.dc[index].lastTimeHeardFrom.minutes);
-		    	masterls.dc[cur_dc_id].lastTimeHeardFrom.seconds = timeinfo->tm_sec;
+		    	msList->dc[cur_dc_id].lastTimeHeardFrom.hours = timeinfo->tm_hour;
+		    	msList->dc[cur_dc_id].lastTimeHeardFrom.minutes = timeinfo->tm_min;
+		    	msList->dc[cur_dc_id].lastTimeHeardFrom.seconds = timeinfo->tm_sec;
 
 			
 		}
                 time(&rawtime);
 	        timeinfo = localtime(&rawtime);
-               //COMMENTED OUT masterls.dc[DC_count].lastTimeHeardFrom =  asctime(timeinfo);
-              //  printf("\nThe number of DCs is %d\n", masterls.numberOfDCs);
 		existDCOrNot = false;//NEW
 		shouldRemove = false;
             }//NEW SECTION FOR UPD MSG
