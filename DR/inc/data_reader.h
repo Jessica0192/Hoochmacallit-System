123cf4a35baf8c2d7f1d2f03034883990292b191
MMalinina <72006658+MMalinina@users.noreply.github.com>
2021-03-22 21:54:26 -0400

    Update data_reader.h

--- DR/inc/data_reader.h
+++ DR/inc/data_reader.h
@@ -1,6 +1,7 @@
 #define FIRST_SLEEP 15
 #define LAST_SLEEP 1.5
 #define TYPE_SERVERMESSAGE		1
+#define MAX_DC_ROLES 	10 
 #define OPERATION_ADD			1
 #define OPERATION_DELETE		2
 #define OPERATION_LIST			3
@@ -23,11 +24,61 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#define MAX_DC_ROLES 10 
 
-//key_t ftok(char* path, int id);
-int msgget(key_t key, int msgFlag);
+
+
+typedef struct  
+{
+	// the following is a requriement of UNIX/Linux
+	long type;
+
+	// now the specifics of our message
+	pid_t machinePID;
+	char msg[100];
+	
+} DCMessage;
+
 //key_t ftok(char* path, int id);
 int msgget(key_t key, int msgFlag);
 //int shmget(key_t key, int memSize, int memFLag);
 void delay(int milliseconds);
+
+typedef struct  
+{
+	int hours;
+	int minutes;
+	int seconds;
+	int time;
+	
+} last_time;
+
+typedef struct  
+{
+	// the following is a requriement of UNIX/Linux
+	long type;
+
+	// now the specifics of our message
+	pid_t dcProcessID;
+	last_time lastTimeHeardFrom;	//to calculate difference use "double difftime ( time_t time2, time_t time1 ); " 
+	
+} DCInfo;
+
+
+typedef struct  
+{
+	// the following is a requriement of UNIX/Linux
+	long type;
+
+	// now the specifics of our message
+	int msgQueueID;
+	int numberOfDCs;
+	DCInfo dc[MAX_DC_ROLES];
+	
+} MasterList;
+
+
+
+
+
+//key_t ftok(char* path, int id);
+int msgget(key_t key, int msgFlag);
