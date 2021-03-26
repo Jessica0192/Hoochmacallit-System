02a83d8285c3d34ca53b2a46d0be3b4097a750a6
MMalinina <72006658+MMalinina@users.noreply.github.com>
2021-03-26 00:36:50 -0400

    Update and rename data_reader.h to dr.h

---
+++ DR/inc/dr.h
@@ -0,0 +1,28 @@
+
+/*
+* FILE          : dr.h
+* PROJECT       : PROG2121 - Assignment #3
+* PROGRAMMER    : Maria Malinina
+* FIRST VERSION : 2020-03-14
+* DESCRIPTION   :
+* This file contains all the #includes, constants and function prototypes
+* that the data reader program uses.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#define FIRST_SLEEP 15
+#define LAST_SLEEP 1.5
+#include "../../Common/inc/common.h"
+
+void removeDC(char* strCount, char*strProcessID, char* rem_dc_log, FILE* log_stream, int status);
+void rearrange(int removalID, int localNumDCs, MasterList* msList, int DC_pids[]);
+void removeLogic(int localNumDCs, MasterList* msList, int removalID, char* strCount, char* strProcessID, int DC_pids[], char* rem_dc_log, FILE* log_stream);
